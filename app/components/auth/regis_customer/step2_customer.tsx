2bc0e1f87d65ed426b0d6dbcbdfb1c7c0d22ae0d
Nazhwa <nazhwanursyabrina@gmail.com>
2023-12-14 16:00:52 +0700

    chorge login

---
+++ app/components/auth/regis_customer/step2_customer.tsx
@@ -0,0 +1,13 @@
+import { DatePicker, Select, } from "antd/lib/index";
+import { Option } from "antd/es/mentions";
+import { Form, Input} from "antd/lib/index"
+import { FormInstance } from "antd";
+
+function CustomerStep2() {
+    return (
+       <div>
+        
+       </div>
+    )
+}
+export default CustomerStep2
